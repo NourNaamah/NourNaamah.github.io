c746fbff42432294878ef7ad9f6fe2e25f9f7e06
ibrahim7a <Ia20@divcodes.com>
2025-11-17 12:11:55 +0300

    initial commit

---
+++ src/components/global/index.ts
@@ -0,0 +1,6 @@
+export { default as NavBar } from "./NavBar";
+export { default as TopLeftImage } from "./TopLeftImage";
+export { default as Transition } from "./Transition";
+export { default as Header } from "./Header";
+export { default as Loader } from "./Loader";
+export { default as FloatingSparkles } from "./FloatingSparkles";
